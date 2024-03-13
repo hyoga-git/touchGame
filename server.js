cf7fdc6c4466db19668cd021df6ab876add8af06
hyouga0 <hyouga0603001@gmail.com>
2024-03-13 12:19:51 +0900

    5

--- server.js
+++ server.js
@@ -10,16 +10,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-
-
-
-app.use(express.static("views"));
-
 app.get("/",(req,res)=>{
     res.render("home.ejs")
     console.log("ルートディレクトリーに入っています")
 })
-
 
 app.get("/touch",(req,res)=>{
     res.render("game.ejs")
@@ -27,7 +21,6 @@ app.get("/touch",(req,res)=>{
 })
 
 app.get("/ranking",(req,res)=>{
-    res.render("ranking.ejs")
     console.log("ランキング表示")
 
     pool.query(
@@ -42,32 +35,23 @@ app.get("/ranking",(req,res)=>{
     );
 });
 
-
-
 app.post("/submit-result",(req,res)=>{
     const playerName=req.body.playerName
     const times=req.body.record
     console.log(`名前: ${playerName} レコード: ${times}`);
-    app.post("/submit-result", (req, res) => {
 
-        console.log(playerName);
-        console.log(times);
-
-        pool.query(
-            'INSERT INTO touch (name,time) VALUES ($1,$2)',[playerName,times],
-            (error, results) => {
-                if (error) {
-                    console.log("データベースにデータを入れれませんでした.", error);
-                    return res.status(500).send("Internal Server Error");
-                }
-                console.log(`名前:${playerName} レコード: ${times}`);
-                res.status(200).send("OK");
+    pool.query(
+        'INSERT INTO touch (name,time) VALUES ($1,$2)',[playerName,times],
+        (error, results) => {
+            if (error) {
+                console.log("データベースにデータを入れれませんでした.", error);
+                return res.status(500).send("Internal Server Error");
             }
-        );
-    });
-
-
-})
+            console.log(`名前:${playerName} レコード: ${times}`);
+            res.status(200).send("OK");
+        }
+    );
+});
 
 app.listen(process.env.PORT || PORT, () => {
     console.log("サーバー起動");
