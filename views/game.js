aaac41e60869f7a8db7d8cceed3d96d72ba7a4fc
hyouga0 <hyouga0603001@gmail.com>
2024-03-19 22:30:21 +0900

    ok

--- views/game.js
+++ views/game.js
@@ -113,10 +113,26 @@ function StopTime() {
     //DBから配列データーを取得してからタイムを比較してそれらから順位を求めていく
     //なのでforEachで回すだけでおｋ
     
-    const touchData= document.getElementById('touchData');
-    console.log(touchData);
- 
-    score.innerHTML=`${playerName}さんは<br>${record}で$位です!!`
+    function StopTime() {
+        // 他のコード...
+    
+        const touchData = JSON.parse(document.getElementById('touchData').dataset.touch);
+        console.log(touchData);
+        let playerRank = 1;
+        const playerTime = record; // プレイヤーのクリアタイムを取得（ここでは record としていますが適切な値に置き換えてください）
+    
+        touchData.forEach(player => {
+            if (player.time < playerTime) { // プレイヤーのクリアタイムと比較
+                playerRank++;
+            } else {
+                score.innerHTML = `${playerName}さんは<br>${record}で${playerRank}位です!!`;
+            }
+        });
+    
+        // 他のコード...
+    }
+    
+
 
 }
 
