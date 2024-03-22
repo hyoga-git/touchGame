62285d746afb1583d476eebe7fcfe8f23667246c
hyouga0 <hyouga0603001@gmail.com>
2024-03-22 18:53:04 +0900

    ok

--- views/game.js
+++ views/game.js
@@ -113,10 +113,6 @@ function StopTime() {
     //DBから配列データーを取得してからタイムを比較してそれらから順位を求めていく
     //なのでforEachで回すだけでおｋ
 
-
-    console.log(touchData)
-
-
     score.innerHTML = `${playerName}さんは<br>${record}で$位です!!`;
 
 
